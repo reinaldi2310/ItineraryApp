f1f1718bec94b83dd399747fe667bce4b4f04656
Rei <reinaldi.maulana@gmail.com>
2020-08-30 21:27:14 +0700

    First commit for pushing to github

---
+++ Itinerary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Itinerary
+//
+//  Created by User on 29/08/20.
+//  Copyright © 2020 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
